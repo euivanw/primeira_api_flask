ff45e80ec944696cef01ccb799ac582dcde39a36
Ivan Wilhelm <contato@ivanwilhelm.dev>
2024-05-02 13:40:05 -0300

    Cadastrar um novo carro

--- app.py
+++ app.py
@@ -3,12 +3,18 @@ from flask import Flask, jsonify, make_response, request
 from db import carros
 
 app = Flask(__name__)
+app.json.sort_keys = False
 
 
 @app.route("/carros", methods=['GET'])
 def get_carros():
     ''' Retorna a lista de carros cadastrada. '''
-    return make_response(jsonify(carros))
+    return make_response(
+        jsonify(
+            message='Lista de carros.',
+            data=carros
+        )
+    )
 
 
 @app.route('/carros', methods=['POST'])
@@ -16,7 +22,12 @@ def create_carro():
     ''' Insere um carro na lista. '''
     carro = request.json
     carros.append(carro)
-    return carro
+    return make_response(
+        jsonify(
+            message='Carro cadastrado com sucesso.',
+            data=carro
+        )
+    )
 
 
 if __name__ == '__main__':
