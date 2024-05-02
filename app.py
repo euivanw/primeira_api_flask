fe6028f5296e06e7fc80e95bda5e4ec19b0ae284
Ivan Wilhelm <contato@ivanwilhelm.dev>
2024-05-02 13:33:55 -0300

    Criar um novo carro e adicionar na lista

--- app.py
+++ app.py
@@ -1,15 +1,23 @@
-""" Importações """
-from flask import Flask, jsonify, make_response
+''' Importações '''
+from flask import Flask, jsonify, make_response, request
 from db import carros
 
 app = Flask(__name__)
 
 
 @app.route("/carros", methods=['GET'])
-def get_carros() -> list:
-    """ Retorna a lista de carros cadastrada. """
+def get_carros():
+    ''' Retorna a lista de carros cadastrada. '''
     return make_response(jsonify(carros))
 
 
-if __name__ == "__main__":
+@app.route('/carros', methods=['POST'])
+def create_carro():
+    ''' Insere um carro na lista. '''
+    carro = request.json
+    carros.append(carro)
+    return carro
+
+
+if __name__ == '__main__':
     app.run()
